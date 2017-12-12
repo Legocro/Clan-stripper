4d8d4586d744165918e9bb362a2b502965d410b0
Legocro <marin.chy@live.com>
2017-12-12 18:11:36 +0100

    New layout

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-window.onload = function(){
-$("#main").on("click" , strip);
+window.onload = function () {
+    $("#main").on("click", strip);
 }
 
 function strip() {
-	let temp = $("#input").val();
-	temp = decode(temp);
-	temp = encode(temp);
-	$("#output").val(temp);
+    let temp = $("#input").val();
+    temp = decode(temp);
+    temp = encode(temp);
+    $("#output").val(temp);
 }
 
 
@@ -26,24 +26,24 @@ function decodeOld(data) {
         txt += result[0][i];
     data = JSON.parse(atob(txt));
     data.type = "old";
-    console.log("Decoded(o): " , data);
+    console.log("Decoded(o): ", data);
     return data
 }
 
 function decodeNew(data) {
-	let result = data.slice(32)
-	data = pako.inflate(atob(result), {to: 'string' });
-	data = JSON.parse(data);
-	data.type = "new"
-	console.log("Decoded(n): " , data);
-	return data;
+    let result = data.slice(32)
+    data = pako.inflate(atob(result), { to: 'string' });
+    data = JSON.parse(data);
+    data.type = "new"
+    console.log("Decoded(n): ", data);
+    return data;
 
 }
 
 function encodeOld(data) {
-	data = wipe(data);
-	console.log("Encoded(o): " , data);
-	data = JSON.stringify(data);
+    data = wipe(data);
+    console.log("Encoded(o): ", data);
+    data = JSON.stringify(data);
     let letters = "0123456789abcdefghijklmnopqrstuvwxyz"
     let newdata = btoa(data);
     let newsprinkle = "";
@@ -56,29 +56,29 @@ function encodeOld(data) {
 }
 
 function encodeNew(data) {
-	data = wipe(data);
-	console.log("Encoded(n): " , data);
-	let hash = "7a990d405d2c6fb93aa8fbb0ec1a3b23";
-	data = JSON.stringify(data);
-	let encodedData = pako.deflate(data, {to: 'string'});
-	return hash + btoa(encodedData);
+    data = wipe(data);
+    console.log("Encoded(n): ", data);
+    let hash = "7a990d405d2c6fb93aa8fbb0ec1a3b23";
+    data = JSON.stringify(data);
+    let encodedData = pako.deflate(data, { to: 'string' });
+    return hash + btoa(encodedData);
 
 }
 
 
 function decode(data) {
-	if (data.indexOf("Fe12NAfA3R6z4k0z") == -1) return decodeNew(data);
-	return decodeOld(data);
+    if (data.indexOf("Fe12NAfA3R6z4k0z") == -1) return decodeNew(data);
+    return decodeOld(data);
 }
 
-function encode(data){
-	data = typeof data == "object" ? data : JSON.parse(data);
-	if (data.type == "old") return encodeOld(data);
-		return encodeNew(data);
+function encode(data) {
+    data = typeof data == "object" ? data : JSON.parse(data);
+    if (data.type == "old") return encodeOld(data);
+    return encodeNew(data);
 }
 
 function wipe(data) {
-	data.type = "kappa";
+    data.type = "kappa";
     data.email = "";
     data.passwordHash = "";
     data.prevLoginTimestamp = 0;
@@ -89,6 +89,6 @@ function wipe(data) {
     data.subscribeEmail = "";
     data.type = undefined;
     delete data.type;
-    console.log(`Wiped:` , data);
+    console.log(`Wiped:`, data);
     return data;
 }
